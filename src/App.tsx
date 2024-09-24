4175f87e47dfee9835da24b5f83f8fee30459d8d
Jesus Martinez <jmartine203@cuc.edu.co>
2024-09-24 13:11:16 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import miLogo from "../src/assets/img/italia.jpg";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={miLogo} alt="Logo React" />
+        <h1>¡¡¡Bienvenido a mi pagina!!!</h1>
+        <p>Este es el primer taller del proyecto de React...</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
